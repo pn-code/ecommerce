0fda7fd1b988a7553ac0a60fba1eedbf11293ea7
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-22 16:48:47 -0700

    Built out Cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,56 +1,89 @@
+import CartItemCard from "@/components/CartItemCard";
 import React from "react";
+const dummyData = [
+    {
+        id: 1,
+        name: "item 1",
+        ratings: [5.0],
+        img: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+        price: 5,
+    },
+    {
+        id: 2,
+        name: "item 2",
+        ratings: [5.0, 4.0],
+        img: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+        price: 5,
+    },
+    {
+        id: 3,
+        name: "item 3",
+        ratings: [5.0],
+        img: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+        price: 5,
+    },
+    {
+        id: 4,
+        name: "item 4",
+        ratings: [5.0],
+        img: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+        price: 5,
+    },
+    {
+        id: 5,
+        name: "item 5",
+        ratings: [5.0],
+        img: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+        price: 5,
+    },
+    {
+        id: 6,
+        name: "item 6",
+        ratings: [5.0],
+        img: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+        price: 5,
+    },
+    {
+        id: 7,
+        name: "item 7",
+        ratings: [5.0],
+        img: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+        price: 5,
+    },
+    {
+        id: 8,
+        name: "item 8",
+        ratings: [5.0],
+        img: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+        price: 5,
+    },
+    {
+        id: 9,
+        name: "item 9",
+        ratings: [5.0],
+        img: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+        price: 5,
+    },
+];
 
-const Cart = (props) => {
-
-    const [ shoppingCart, setShoppingCart ] = React.useState([])
-    const [ totalPrice, setTotalPrice ] = React.useState(0)
-
-    React.useEffect(() => {
-        setShoppingCart(props.shoppingCart)
-
-    }, [props.shoppingCart])
-
-    React.useEffect(() => {
-        setTotalPrice(getTotal())
-
-    }, [shoppingCart])
-
-    function getTotal() {
-        let result;
-        if ( shoppingCart.length > 0){
-            result = shoppingCart.map(item => (item.price * item.quantity))
-                .reduce(((total, num) => total + num))
-        }
-        return result;
-    }
-
-    const shoppingCartElements = shoppingCart.map( item => 
-        <div className="shopping-cart-item">
-            <div className="left-shopping-cart-item">
-                <img src={ item.imgSrc } alt={ item.title }/>
-                <div className="item-information">
-                    <p> { item.title } </p>
-                    <p> Rating : { item.rating } / 5.0 </p>
-                    <p> Price : ${ item.price } x { item.quantity }</p>
-                    <p> = ${ item.price * item.quantity } </p>
-                </div>
-            </div>
-
-            <div className="right-shopping-cart-item">
-                <button> Remove Items </button>
-            </div>
-        </div>
-    )
-
+const Cart = () => {
     return (
-        <div className="Cart">
-            <h1> Your Cart: </h1>
-            { shoppingCartElements }
-            <h2>Total Price : ${ totalPrice } </h2>
-            <button onClick={ props.handleClearCart }> Clear Cart </button>
-            <button onClick={ props.handleCheckOut }> Check Out </button>
-        </div>
-    )
-}
+        <>
+            <header className="px-4 text-xl font-bold">
+                <h1>Your Cart</h1>
+            </header>
+            <main>
+                {/* Left section with all cart items */}
+                <section>
+                    {dummyData.map((product) => (
+                        <CartItemCard product={product} />
+                    ))}
+                </section>
+                <section>{/* Render Cart Items Here */}</section>
+                {/* Right section with payment/proceed to checkout */}
+            </main>
+        </>
+    );
+};
 
 export default Cart;
