940f9db304311866c9c57f113167fd7cbd6fa027
Philip Nguyen <philipnguyen953@gmail.com>
2023-10-06 18:49:39 -0700

    Fixed type errors

--- components/main/SpecialSection.tsx
+++ components/main/SpecialSection.tsx
@@ -6,13 +6,13 @@ export default function SpecialSection() {
   return (
     <div className="flex flex-col gap-4">
       <header>
-        <h2 className="text-lg font-semibold">Today's Special Items</h2>
+        <h2 className="text-lg font-semibold">Today&apos;s Special Items</h2>
         <p className="text-sm text-slate-800">Buy now before items run out!</p>
       </header>
 
       <div className="w-full flex gap-2 flex-wrap">
         {products.map((product) => (
-          <ProductCard {...product} />
+          <ProductCard key={product.id} {...product} />
         ))}
       </div>
     </div>
