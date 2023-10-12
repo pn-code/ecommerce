155cba98dc23bc09a87bc0c8b7c724668d575bdd
Philip Nguyen <philipnguyen953@gmail.com>
2023-10-11 21:56:29 -0700

    Created getProduct helper

---
+++ helpers/products/getProduct.ts
@@ -0,0 +1,14 @@
+import { prisma } from "@/lib/db";
+
+export async function getProduct(id: number) {
+  try {
+    const product = await prisma.product.findFirst({
+      where: {
+        id: id,
+      },
+    });
+    return product;
+  } catch (error: any) {
+    console.error(error.message);
+  }
+}
