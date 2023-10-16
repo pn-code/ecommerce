d5097e9e53734aae688a06d95066f3bb338a0cb3
Philip Nguyen <philipnguyen953@gmail.com>
2023-10-16 15:32:09 -0700

    Get orders w/ orderItems & products in desc order

--- helpers/orders/getOrders.ts
+++ helpers/orders/getOrders.ts
@@ -2,23 +2,33 @@ import { prisma } from "@/lib/db";
 import { currentUser } from "@clerk/nextjs";
 
 export async function getOrders() {
-    try {
-        const user = await currentUser();
+  try {
+    const user = await currentUser();
 
-        if (!user) {
-            throw new Error("No user found.");
-        }
-
-        const orders = await prisma.order.findMany({
-            where: {
-                user_id: user.id,
-            },
-        });
-
-        if (!orders) return [];
-
-        return orders;
-    } catch (error: any) {
-        console.error("ORDERS/GET: ", error.message);
+    if (!user) {
+      throw new Error("No user found.");
     }
+
+    const orders = await prisma.order.findMany({
+      where: {
+        user_id: user.id,
+      },
+      include: {
+        orderItems: {
+          include: {
+            product: true,
+          },
+        },
+      },
+      orderBy: {
+        created_at: "desc",
+      },
+    });
+
+    if (!orders) return [];
+
+    return orders;
+  } catch (error: any) {
+    console.error("ORDERS/GET: ", error.message);
+  }
 }
