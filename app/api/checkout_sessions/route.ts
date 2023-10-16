a4937a84064c1e4d6f75e5d169d5e309d517c424
Philip Nguyen <philipnguyen953@gmail.com>
2023-10-15 23:41:53 -0700

    Added shipping address collection

--- app/api/checkout_sessions/route.ts
+++ app/api/checkout_sessions/route.ts
@@ -4,22 +4,25 @@ import { createLineItems } from "@/helpers/carts/createLineItems";
 import { stripe } from "@/lib/stripeClient";
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  try {
-    const carts = (await req.json()) as CartItem[];
-    const lineItems = createLineItems(carts);
+    try {
+        const carts = (await req.json()) as CartItem[];
+        const lineItems = createLineItems(carts);
 
-    const HOST_URL = process.env.HOST_URL;
+        const HOST_URL = process.env.HOST_URL;
 
-    // Create Checkout Sessions from body params.
-    const session = await stripe.checkout.sessions.create({
-      line_items: lineItems,
-      mode: "payment",
-      success_url: `${HOST_URL}/orders/?success=true`,
-      cancel_url: `${HOST_URL}/orders/?canceled=true`,
-    });
+        // Create Checkout Sessions from body params.
+        const session = await stripe.checkout.sessions.create({
+            line_items: lineItems,
+            mode: "payment",
+            success_url: `${HOST_URL}/orders/?success=true`,
+            cancel_url: `${HOST_URL}/orders/?canceled=true`,
+            shipping_address_collection: {
+                allowed_countries: ["US"],
+            },
+        });
 
-    return NextResponse.json(session, { status: 200 });
-  } catch (error: any) {
-    return NextResponse.json("CHECKOUT_SESSIONS/POST: ", error.message);
-  }
+        return NextResponse.json(session, { status: 201 });
+    } catch (error: any) {
+        return NextResponse.json("CHECKOUT_SESSIONS/POST: ", error.message);
+    }
 }
