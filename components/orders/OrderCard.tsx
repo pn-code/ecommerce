c5ab48f77cf180ce74468f1f059567307b4e0cf1
Philip Nguyen <philipnguyen953@gmail.com>
2023-10-16 17:24:51 -0700

    Updated status badge colors

--- components/orders/OrderCard.tsx
+++ components/orders/OrderCard.tsx
@@ -28,7 +28,15 @@ export default function OrderCard({ order }: OrderCardProps) {
         <CardTitle className="flex justify-between gap-4">
           <h3>Order #{order.id}</h3>
           <div className="flex gap-2 items-center">
-            <h3 className="text-xs bg-blue-500 py-0.5 px-3 rounded-full text-white">
+            <h3
+              className={cn(
+                "text-xs py-0.5 px-3 rounded-full text-white",
+                order.delivery_status === "PROCESSING" && "bg-blue-500",
+                order.delivery_status === "SHIPPING" && "bg-indigo-600",
+                order.delivery_status === "DELIVERED" && "bg-red-600",
+                order.delivery_status === "CANCELLED" && "bg-gray-600"
+              )}
+            >
               {order.delivery_status}
             </h3>
             <p className="font-semibold">${order.total_amount * 0.01} USD</p>
