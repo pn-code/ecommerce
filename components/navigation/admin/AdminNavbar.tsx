149ac71dbbae6c9ef4477570883b68036329f730
Philip Nguyen <philipnguyen953@gmail.com>
2023-10-17 21:29:01 -0700

    Improved admin nav UI

--- components/navigation/admin/AdminNavbar.tsx
+++ components/navigation/admin/AdminNavbar.tsx
@@ -1,23 +1,22 @@
 import Image from "next/image";
 
 import Logo from "@/public/meat_logo.svg";
-import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import Link from "next/link";
 
 export default function AdminNavbar() {
   return (
-    <nav className="w-full flex justify-between h-20 items-center px-4">
-      <header className="flex gap-2 items-center">
+    <nav className="w-full flex flex-col md:flex-row justify-between md:h-20 mb-2 md:mb-0 items-center px-4">
+      <header className="flex gap-2 items-center p-1 md:p-0">
         <Image
-          className="p-1 rounded-full h-16 w-16"
+          className="p-1 rounded-full h-10 w-10 md:h-16 md:w-16"
           src={Logo}
           alt="Logo"
           height={100}
           width={100}
         />
-        <h1 className="text-xl font-semibold">Uncle Ben&apos;s Admin Page</h1>
+        <h1 className="hidden md:flex text-xl font-semibold">Uncle Ben&apos;s Admin Page</h1>
+        <h1 className="md:hidden text-lg font-semibold">UBMF Admin</h1>
       </header>
 
       {/* Content */}
