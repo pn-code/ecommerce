e3c0bab554a77162fae947c318876c2310df175a
Philip Nguyen <philipnguyen953@gmail.com>
2023-10-20 18:01:42 -0700

    Removed admin privileges for billboard get

--- helpers/billboards/getBillboards.ts
+++ helpers/billboards/getBillboards.ts
@@ -1,12 +1,7 @@
 import { prisma } from "@/lib/db";
-import { isCurrentUserAdmin } from "../isCurrentUserAdmin";
 
 export async function getBillboards() {
   try {
-    const admin = await isCurrentUserAdmin();
-
-    if (!admin) return false;
-
     const billboards = await prisma.billboard.findMany({
       orderBy: {
         order: "asc",
