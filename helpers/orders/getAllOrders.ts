741de5f829f07a2780201b34825462027060b6cb
Philip Nguyen <philipnguyen953@gmail.com>
2023-11-06 17:42:48 -0800

    Created getAllOrders for admin page

---
+++ helpers/orders/getAllOrders.ts
@@ -0,0 +1,29 @@
+import { prisma } from "@/lib/db";
+import { isCurrentUserAdmin } from "../isCurrentUserAdmin";
+
+export async function getAllOrders() {
+  try {
+    const admin = isCurrentUserAdmin();
+
+    if (!admin) {
+      throw new Error("ALL ORDERS/GET: USER IS NOT ADMIN.");
+    }
+
+    const orders = await prisma.order.findMany({
+      orderBy: {
+        created_at: "desc",
+      },
+      include: {
+        orderItems: {
+          include: {
+            product: true,
+          },
+        },
+      },
+    });
+
+    return orders;
+  } catch (error: any) {
+    console.error("ALL ORDERS/GET: ", error.message);
+  }
+}
